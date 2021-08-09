3c4c59a135568a9ab074eaceb7c1642c77a5653d
何炀杰 <450951286@qq.com>
2021-08-09 15:23:46 +0800

    no message

---
+++ JetpackMvvm/src/main/java/com/icare/jetpackmvvm/widget/cardview/ICareAlphaViewInf.java
@@ -0,0 +1,19 @@
+package com.icare.jetpackmvvm.widget.cardview;
+
+public interface ICareAlphaViewInf {
+
+    /**
+     * 设置是否要在 press 时改变透明度
+     *
+     * @param changeAlphaWhenPress 是否要在 press 时改变透明度
+     */
+    void setChangeAlphaWhenPress(boolean changeAlphaWhenPress);
+
+    /**
+     * 设置是否要在 disabled 时改变透明度
+     *
+     * @param changeAlphaWhenDisable 是否要在 disabled 时改变透明度
+     */
+    void setChangeAlphaWhenDisable(boolean changeAlphaWhenDisable);
+
+}
