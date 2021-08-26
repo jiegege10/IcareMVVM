b7b1e11baa4248942986dccc5135878c7064f45f
何炀杰 <450951286@qq.com>
2021-08-26 11:49:39 +0800

    no message

--- app/src/main/java/com/icare/demo/MainActivity.kt
+++ app/src/main/java/com/icare/demo/MainActivity.kt
@@ -1,91 +1,13 @@
 package com.icare.demo
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
-import com.google.gson.Gson
-import com.icare.demo.a.TestDialog
-import com.icare.demo.databinding.ActivityMainBinding
-import com.icare.mvvm.ext.init
-import com.icare.mvvm.ext.parseState
-import com.icare.mvvm.ext.setSingleClickListener
-import com.icare.mvvm.util.LogUtils
-import com.icare.mvvm.util.Preference
-import kotlinx.android.synthetic.main.activity_main.*
-
-class MainActivity : BaseActivity<RequestAriticleViewModel, ActivityMainBinding>() {
-    //请求数据ViewModel
-    private var AAA: Boolean by Preference("Constant.SP_GOOGLE2FA_SECRET", false)
-    private val requestHomeViewModel: RequestAriticleViewModel by viewModels()
-    override fun layoutId(): Int = R.layout.activity_main
-    override fun initView(savedInstanceState: Bundle?) {
-        addLoadingObserve(requestHomeViewModel)
-        tvtitle.setLeftOnClickListener {
-
-        }
-        tv_aaa.setOnSuperTextViewClickListener {
-            LogUtils.debugInfo("XXXXX")
-        }
-        tv_time.setOnSuperTextViewClickListener {
-//            LogUtils.debugInfo("XXXXX")
-//            requestHomeViewModel.getShareData()
+import androidx.appcompat.app.AppCompatActivity
 
 
-        }
-
-        tv_aaa.setEditString("xxxxasdasdasdasd")
-
-        tvtitle.setRightIcon(R.mipmap.icon_delete_gray)
-        tvtitle.setRightIconVisibility(true)
-        tvtitle.init("", this)
-        mDatabind.tv.setSingleClickListener {
-            showProgressDialog("测试") {
-                LogUtils.debugInfo("取消了")
-            }
-//            LogUtils.debugInfo( mDatabind.tvAaa.editText.text.toString())
-//            requestHomeViewModel.getShareData()
-//            try {
-//            var dialog = TestDialog(this)
-//            dialog.show()
-//                requestHomeViewModel.getShareData()
-//            showLoading()
-//            }catch (e:Exception){
-//                Log.e("XXXXXXXXX",e.toString())
-//            }
-//            HttpRequest.download(this,"https://app.pcpsoo.com//uploads//shops//20210322//8530ea5f14cfa8d82d5342057bb3b575.jpg",
-//                File("/sdcard/xx.png"),
-//                object :FileDownloadCallback(){
-//                    override fun onStart() {
-//                        super.onStart()
-//                    }
-//
-//                    override fun onDone() {
-//                        super.onDone()
-//                        LogUtils.debugInfo("下载完成")
-//                    }
-//
-//                    override fun onFailure() {
-//                        super.onFailure()
-//                    }
-//
-//                    override fun onProgress(progress: Int, networkSpeed: Long) {
-//                        super.onProgress(progress, networkSpeed)
-//                        LogUtils.debugInfo("下载中：$progress")
-//                    }
-//                })
-        }
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
-
-    override fun createObserver() {
-        super.createObserver()
-        requestHomeViewModel.bannerData.observe(this) {
-            parseState(it, {
-                Log.d("XXXXXXXX", Gson().toJson(it))
-            },{
-                showToast(it.errorMsg)
-            })
-        }
-    }
-
 
 }
